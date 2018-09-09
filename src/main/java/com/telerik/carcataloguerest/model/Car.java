9777f2ccbb05a11f26aa5e630147d0063e709bdd
Slavi <slavi_14@abv.bg>
2018-09-09 18:07:31 +0300

    model

--- src/main/java/com/telerik/carcataloguerest/model/Car.java
+++ src/main/java/com/telerik/carcataloguerest/model/Car.java
@@ -1,4 +1,43 @@
 package com.telerik.carcataloguerest.model;
 
 public class Car {
+    private int id;
+    private String make;
+    private String model;
+    private int power;
+    private int cubicCapacity;
+    private String imgUrl;
+
+    public Car(int id, String make, String model, int power, int cubicCapacity,String imgUrl){
+        this.id=id;
+        this.make=make;
+        this.model=model;
+        this.power=power;
+        this.cubicCapacity=cubicCapacity;
+        this.imgUrl=imgUrl;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public int getCubicCapacity() {
+        return cubicCapacity;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
 }
