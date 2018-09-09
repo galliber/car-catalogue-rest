a3b966d1ca375e2fd9c0b881e7089d2be3c84587
Galiber <denis.epavlov96@gmail.com>
2018-09-09 17:40:47 +0300

    Initial commit

---
+++ src/main/java/com/telerik/carcataloguerest/CarCatalogueRestApplication.java
@@ -0,0 +1,12 @@
+package com.telerik.carcataloguerest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarCatalogueRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarCatalogueRestApplication.class, args);
+    }
+}
---
+++ src/test/java/com/telerik/carcataloguerest/CarCatalogueRestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.telerik.carcataloguerest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CarCatalogueRestApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
