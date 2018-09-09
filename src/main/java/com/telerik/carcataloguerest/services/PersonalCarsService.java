73c78a8c1e9026e142525ee67c29bce9293b39cb
Slavi <slavi_14@abv.bg>
2018-09-09 18:31:42 +0300

    services-personal

--- src/main/java/com/telerik/carcataloguerest/services/PersonalCarsService.java
+++ src/main/java/com/telerik/carcataloguerest/services/PersonalCarsService.java
@@ -1,4 +1,45 @@
 package com.telerik.carcataloguerest.services;
 
+
+import com.telerik.carcataloguerest.model.Car;
+import com.telerik.carcataloguerest.repositories.CarRepository;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Qualifier("personal")
 public class PersonalCarsService implements CarService {
+
+    private final CarRepository repository;
+
+    public PersonalCarsService(CarRepository repository){
+        this.repository=repository;
+    }
+
+    @Override
+    public void createCar(Car car) {
+        repository.createCar(car);
+    }
+
+    @Override
+    public List<Car> getCars() {
+        return repository.getCars();
+    }
+
+    @Override
+    public Car getCarById(int id) {
+        return repository.getCarById(id);
+    }
+
+    @Override
+    public void updateCar(int id, Car car) {
+        repository.updateCar(id, car);
+    }
+
+    @Override
+    public void deleteCar(int id) {
+        repository.deleteCar(id);
+    }
 }
