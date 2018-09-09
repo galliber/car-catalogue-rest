4ab9137d4209e142493e38cdc2bdce9a5f31b11d
Slavi <slavi_14@abv.bg>
2018-09-09 18:18:30 +0300

    repository

--- src/main/java/com/telerik/carcataloguerest/repositories/CarRepository.java
+++ src/main/java/com/telerik/carcataloguerest/repositories/CarRepository.java
@@ -1,4 +1,10 @@
 package com.telerik.carcataloguerest.repositories;
-
+import com.telerik.carcataloguerest.model.Car;
+import java.util.List;
 public interface CarRepository {
+    void createCar(Car car);
+    List<Car> getCars();
+    Car getCarById(int id);
+    void updateCar(int id, Car car);
+    void deleteCar(int id);
 }
--- src/main/java/com/telerik/carcataloguerest/repositories/InMemoryCarRepository.java
+++ src/main/java/com/telerik/carcataloguerest/repositories/InMemoryCarRepository.java
@@ -1,4 +1,50 @@
 package com.telerik.carcataloguerest.repositories;
 
+import com.telerik.carcataloguerest.model.Car;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+@Scope("prototype")
 public class InMemoryCarRepository implements CarRepository {
+
+    private List<Car> cars=new ArrayList();
+    public InMemoryCarRepository(){
+        cars.add(new Car(0, "Audi", "A3", 143, 2000,""));
+        cars.add(new Car(1, "Audi", "S5", 354, 3000,""));
+        cars.add(new Car(2, "Mercedess-Benz", "S600", 5900, 600,""));
+    }
+
+    @Override
+    public void createCar(Car car) {
+        cars.add(car);
+    }
+
+    @Override
+    public List<Car> getCars() {
+        return cars;
+    }
+
+    @Override
+    public Car getCarById(int id) {
+        return cars.stream().filter(car->car.getId()==id).findFirst().orElse(null);
+    }
+
+    @Override
+    public void updateCar(int id, Car car) {
+        cars.set(cars.indexOf(cars.stream().filter(car1->car1.getId()==id).findFirst().orElse(null)), car);
+    }
+
+    @Override
+    public void deleteCar(int id) {
+        if(!cars.isEmpty()) {
+            Car car=cars.stream().filter(car1->car1.getId()==id).findFirst().orElse(null);
+            if(car!=null) {
+                cars.remove(cars.indexOf(car));
+            }
+        }
+    }
 }
