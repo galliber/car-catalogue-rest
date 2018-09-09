1e1c9458a7156e7cb0a1b61b0421d60ae8602273
Slavi <slavi_14@abv.bg>
2018-09-09 18:27:39 +0300

    services

--- src/main/java/com/telerik/carcataloguerest/services/CarService.java
+++ src/main/java/com/telerik/carcataloguerest/services/CarService.java
@@ -1,4 +1,13 @@
 package com.telerik.carcataloguerest.services;
 
+import com.telerik.carcataloguerest.model.Car;
+
+import java.util.List;
+
 public interface CarService {
+    void createCar(Car car);
+    List<Car> getCars();
+    Car getCarById(int id);
+    void updateCar(int id, Car car);
+    void deleteCar(int id);
 }
