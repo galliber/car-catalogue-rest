9c7e4bc1e14255b5b20d84505216d48d338a48e4
Galiber <denis.epavlov96@gmail.com>
2018-09-10 09:14:26 +0300

    Added default picture.

--- src/main/java/com/telerik/carcataloguerest/repositories/InMemoryCarRepository.java
+++ src/main/java/com/telerik/carcataloguerest/repositories/InMemoryCarRepository.java
@@ -13,14 +13,20 @@ public class InMemoryCarRepository implements CarRepository {
 
     private List<Car> cars=new ArrayList();
     public InMemoryCarRepository(){
-        cars.add(new Car(0, "Audi", "A3", 143, 2000,""));
-        cars.add(new Car(1, "Audi", "S5", 354, 3000,""));
-        cars.add(new Car(2, "Mercedess-Benz", "S600", 5900, 600,""));
+        cars.add(new Car(0, "Audi", "A3", 143, 2000,"https://assets.gocar.be/picserver1/userdata/1/25733/VmlTxhgqF/kfz46260792_capture7.jpg"));
+        cars.add(new Car(1, "Audi", "S5", 354, 3000,"https://www.autocar.co.uk/sites/autocar.co.uk/files/styles/gallery_slide/public/images/car-reviews/first-drives/legacy/audi-s5-uk-0818.jpg?itok=TyKkb6xu"));
+        cars.add(new Car(2, "Mercedes-Benz", "S600", 5900, 600,"https://d3lp4xedbqa8a5.cloudfront.net/imagegen/max/ccr/1023/-/s3/digital-cougar-assets/motor/2015/10/26/48222/Mercedes-Maybach-S600-cover-2.jpg"));
     }
 
     @Override
     public void createCar(Car car) {
-        cars.add(car);
+        Car toAdd=null;
+        if(car.getImgUrl().length()<25||car.getImgUrl()==""){
+            toAdd=new Car(car.getId(), car.getMake(), car.getModel(), car.getPower(), car.getCubicCapacity(), "https://increasify.com.au/wp-content/uploads/2016/08/default-image.png");
+        }
+        else
+            toAdd=car;
+        cars.add(toAdd);
     }
 
     @Override
